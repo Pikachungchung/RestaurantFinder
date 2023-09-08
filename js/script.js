4910615b9613342b66be6cfe18b37c073fd76e29
Hong Kong Omsi Studio <130741690+Pikachungchung@users.noreply.github.com>
2023-09-09 02:24:34 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('bagButton').addEventListener('click', function() {
+    alert('Bag button clicked');
+});
+
+document.getElementById('loginButton').addEventListener('click', function() {
+    alert('Login/Register button clicked');
+});
+
+document.getElementById('globeButton').addEventListener('click', function() {
+    alert('Globe button clicked');
+});
---
+++ js/website-scrpit.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded',function()
+{var image = document.querySelector('.restaurant-thumbnail img');
+var zoomFactor = 1.2})
